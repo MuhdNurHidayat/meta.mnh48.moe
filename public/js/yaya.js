35d7eb3f2c97ed9923936a1dff1dfd8f42140b63
Yaya <admin@mnh48.moe>
2020-03-17 12:24:04 +0800

    rearrange js

--- public/js/yaya.js
+++ public/js/yaya.js
@@ -20,14 +20,14 @@ function showEnglish() {
 function showMalayL() {
   var i, contentEn, contentMs, contentJawi;
   
-  contentEn = document.querySelectorAll('[lang="en-MY"]');
-  for (i = 0; i < contentEn.length; i++) {
-    contentEn[i].style.display = "none";
-  }
-  
   contentMs = document.querySelectorAll('[lang="ms"]');
   for (i = 0; i < contentMs.length; i++) {
     contentMs[i].style.display = "block";
+  }
+  
+  contentEn = document.querySelectorAll('[lang="en-MY"]');
+  for (i = 0; i < contentEn.length; i++) {
+    contentEn[i].style.display = "none";
   }
   
   contentJawi = document.querySelectorAll('[lang="ms-Arab"]');
@@ -39,6 +39,11 @@ function showMalayL() {
 function showMalayA() {
   var i, contentEn, contentMs, contentJawi;
   
+  contentJawi = document.querySelectorAll('[lang="ms-Arab"]');
+  for (i = 0; i < contentJawi.length; i++) {
+    contentJawi[i].style.display = "block";
+  }
+  
   contentEn = document.querySelectorAll('[lang="en-MY"]');
   for (i = 0; i < contentEn.length; i++) {
     contentEn[i].style.display = "none";
@@ -47,11 +52,6 @@ function showMalayA() {
   contentMs = document.querySelectorAll('[lang="ms"]');
   for (i = 0; i < contentMs.length; i++) {
     contentMs[i].style.display = "none";
-  }
-  
-  contentJawi = document.querySelectorAll('[lang="ms-Arab"]');
-  for (i = 0; i < contentJawi.length; i++) {
-    contentJawi[i].style.display = "block";
   }
 }
 
