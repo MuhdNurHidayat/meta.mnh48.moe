7f7ba28e3cce24d4bf755bfc20109a16ef62b507
Yaya <admin@mnh48.moe>
2020-03-17 11:30:43 +0800

    fix language tag

--- index.md
+++ index.md
@@ -4,22 +4,22 @@ layout: default
 ---
 
 ## Meta MNH48
-{: lang="en"}
+{: lang="en-MY"}
 
 Meta information of MNH48
-{: lang="en"}
+{: lang="en-MY"}
 
 ### What is this website doing?
-{: lang="en"}
+{: lang="en-MY"}
 
 This website displays information of the persona behind the MNH48 group of websites, basically it shows information of Yaya in further details. This website is hosted on [GitHub Page](https://github.com/MuhdNurHidayat/meta.mnh48.moe) to be transparent of what I've done but the contents itself are still copyrighted by myself and not for reuse without permission.
-{: lang="en"}
+{: lang="en-MY"}
 
 ### What is the theme used on the website?
-{: lang="en"}
+{: lang="en-MY"}
 
 The theme used is [Hyde](https://github.com/poole/hyde) available under the MIT license. The modifications I've done to the theme and/or styling I've done to the website are also released under the MIT license, however all other contents unrelated to the theme and/or styling of the website are not released for reuse without permission.
-{: lang="en"}
+{: lang="en-MY"}
 
 ## Meta MNH48
 {: lang="ms"}
--- public/js/yaya.js
+++ public/js/yaya.js
@@ -1,9 +1,9 @@
 function showEnglish() {
   var i, contentEn, contentMs, contentJawi;
   
-  contentEn = document.querySelectorAll('[lang="en"]');
+  contentEn = document.querySelectorAll('[lang="en-MY"]');
   for (i = 0; i < contentEn.length; i++) {
-    contentEn[i].style.display = "inline";
+    contentEn[i].style.display = "block";
   }
   
   contentMs = document.querySelectorAll('[lang="ms"]');
@@ -20,14 +20,14 @@ function showEnglish() {
 function showMalayL() {
   var i, contentEn, contentMs, contentJawi;
   
-  contentEn = document.querySelectorAll('[lang="en"]');
+  contentEn = document.querySelectorAll('[lang="en-MY"]');
   for (i = 0; i < contentEn.length; i++) {
     contentEn[i].style.display = "none";
   }
   
   contentMs = document.querySelectorAll('[lang="ms"]');
   for (i = 0; i < contentMs.length; i++) {
-    contentMs[i].style.display = "inline";
+    contentMs[i].style.display = "block";
   }
   
   contentJawi = document.querySelectorAll('[lang="ms-Arab"]');
@@ -39,7 +39,7 @@ function showMalayL() {
 function showMalayA() {
   var i, contentEn, contentMs, contentJawi;
   
-  contentEn = document.querySelectorAll('[lang="en"]');
+  contentEn = document.querySelectorAll('[lang="en-MY"]');
   for (i = 0; i < contentEn.length; i++) {
     contentEn[i].style.display = "none";
   }
@@ -51,7 +51,7 @@ function showMalayA() {
   
   contentJawi = document.querySelectorAll('[lang="ms-Arab"]');
   for (i = 0; i < contentJawi.length; i++) {
-    contentJawi[i].style.display = "inline";
+    contentJawi[i].style.display = "block";
   }
 }
 
